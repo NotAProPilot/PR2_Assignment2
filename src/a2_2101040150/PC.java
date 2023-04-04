e13fc4d3ea0a4be1793dbccc9658dcc9fb2519f8
NotAProPilot <buileminhquang@gmail.com>
2023-04-04 14:12:25 +0700

    I have no idea what I'm writing.

---
+++ src/a2_2101040150/PC.java
@@ -0,0 +1,31 @@
+package a2_2101040150;
+/*
+ * Import all the necessary package
+ */
+import java.util.Vector;
+
+import utils.DOpt;
+import utils.DomainConstraint;
+import utils.OptType;
+import utils.collections.Collection;
+
+/**
+ * (HEADER SPECIFICATIONS)
+ * @overview A PC (personal computer) is a multipurpose microcomputer
+ * whose size, capabilities, and price make it feasible for individual use.
+ * @attributes
+ * model String  String
+ * year  Integer int
+ * manufacture String String
+ * comps Set Set
+ * @object A typical PC is PC = < md, y, m, c >
+ *         where md = model (md), year (y), manufacture (m) and comps (c)
+ * @abstract_properties (a short description of the table):
+ * mutable(md) = true /\ optional (md) = false /\ length (md) = 20
+ * mutable(y) = true /\ optional (m) = false /\ length (m) = 20
+ */
+public class PC {
+    public static void main (String[]args) {
+        System.out.println("FUCKKK");
+    }
+}
