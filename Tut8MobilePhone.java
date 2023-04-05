11abf443496825d867361f116b4bcf24475222e0
NotAProPilot <113848893+NotAProPilot@users.noreply.github.com>
2023-04-05 08:12:13 +0700

    Create Tut8MobilePhone.java

---
+++ Tut8MobilePhone.java
@@ -0,0 +1,21 @@
+/**
+ * @overview: represent each mobile phone
+ * @attributes:
+ * manName      String      String
+ * model        String      String
+ * color        Character   char
+ * year         Integer     int
+ * guaranteed   Boolean     boolean
+ * @object a typical phone is <mN, m, c, y, g>, where manName(mN), model(m), color(c), year (y), guaranteed(g)
+ */
+public class MobilePhone {
+    @DomainConstraint(type = "String", mutable = false, optional = true)
+    private String manName;
+    @DomainConstraint
+    private String model;
+    @DomainConstraint
+    private char color;
+    @DomainConstraint
+    private int year;
+    @DomainConstraint
+    private boolean guaranteed;
